56a7cdaa03c986674762b93c13e8758753b14a4c
monfrair <williamsorrentino@gmail.com>
2019-11-17 14:32:42 -0500

    building more routes

--- app.js
+++ app.js
@@ -20,12 +20,22 @@ const tours = JSON.parse(
 fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {res.json
-res.status(200).json({ 
+app.get('/api/v1/tours/:id', (req, res) => {res.json
+console.log(req.params);
+const id = req.params.id * 1;
+const tour = tours.find(el => el.id === id);
+
+
+if(!tour) {
+    return res.status(404).json({
+        status: 'fail',
+        message: 'Invalid ID'
+    });
+}
+    res.status(200).json({ 
     status: 'success',
-    results: tours.length,
     data: {
-        tours
+        tour
     }
 });
 });
