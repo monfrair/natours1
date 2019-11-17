628010561a9c7685e760b1851f1581bc7e240ca0
monfrair <williamsorrentino@gmail.com>
2019-11-17 15:53:14 -0500

    working out the issues

--- app.js
+++ app.js
@@ -4,41 +4,27 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-// app.get('/', (req, res) => {
-//  res.status(200).json({ message: 'hello from server side', app: 'natours1'});
-// });
-// app.post('/', (req, res) => {
-//     res.send('you can post to this endpoint....');
-// })
-// const port = 3000;
 
-// app.listen(port, () => {
-//     console.log(`app running on port ${port}...`);
+const getAllTours = (req, res) => {res.json
+    console.log(req.params);
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
+    
+        res.status(200).json({ 
+        status: 'success',
+        data: {
+            tours
+        }
+    });
+};
 
-// });
+
+
 const tours = JSON.parse(
 fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours/:id', (req, res) => {res.json
-console.log(req.params);
-const id = req.params.id * 1;
-const tour = tours.find(el => el.id === id);
-
-
-// if(!tour) {
-//     return res.status(404).json({
-//         status: 'fail',
-//         message: 'Invalid ID'
-//     });
-// }
-    res.status(200).json({ 
-    status: 'success',
-    data: {
-        tour
-    }
-});
-});
+app.get('/api/v1/tours/:id', getAllTours);
 
 app.post('/api/v1/tours', (req, res) => {
 // console.log(req.body);
