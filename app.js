ad23c6903e64d683a61d3ebbdf7669a8244f8569
monfrair <williamsorrentino@gmail.com>
2019-11-17 13:58:51 -0500

    building routes

--- app.js
+++ app.js
@@ -1,16 +1,61 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
-app.get('/', (req, res) => {
- res.status(200).json({ message: 'hello from server side', app: 'natours1'});
+
+app.use(express.json());
+// app.get('/', (req, res) => {
+//  res.status(200).json({ message: 'hello from server side', app: 'natours1'});
+// });
+// app.post('/', (req, res) => {
+//     res.send('you can post to this endpoint....');
+// })
+// const port = 3000;
+
+// app.listen(port, () => {
+//     console.log(`app running on port ${port}...`);
+
+// });
+const tours = JSON.parse(
+fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {res.json
+res.status(200).json({ 
+    status: 'success',
+    results: tours.length,
+    data: {
+        tours
+    }
 });
-app.post('/', (req, res) => {
-    res.send('you can post to this endpoint....');
-})
+});
+
+app.post('/api/v1/tours', (req, res) => {
+// console.log(req.body);
+const newId = tours[tours.length - 1].id + 1;
+const newTour = Object.assign({ id: newId }, req.body);
+
+tours.push(newTour);
+fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, 
+JSON.stringify(tours), 
+err => {
+    res.status(201).json({
+        status: 'success',
+        data: {
+            tour: newTour
+        }
+    });
+
+}
+);
+
+
+});
+
+
 const port = 3000;
 
 app.listen(port, () => {
     console.log(`app running on port ${port}...`);
 
 });
-
