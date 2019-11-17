a9fefead251a84e63496bc618e595b318cbc7f78
monfrair <williamsorrentino@gmail.com>
2019-11-17 11:59:31 -0500

    initial commit for project
    adding the initial files for the repo

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => {
+ res.status(200).json({ message: 'hello from server side', app: 'natours1'});
+});
+app.post('/', (req, res) => {
+    res.send('you can post to this endpoint....');
+})
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`app running on port ${port}...`);
+
+});
+
