54b502fcf0bf581fa3d5580e05b5062db887f90b
William Sorrentino <williamsorrentino@gmail.com>
2020-04-04 16:51:15 -0400

    building routes

--- app.js
+++ app.js
@@ -10,16 +10,17 @@ const app = express();
 // 1. Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 //global middleware declared prior to routes
 app.use((req, res, next) => {
-	console.log('Hello from middleware');
-	next();
+  console.log('Hello from middleware');
+  next();
 });
 
 app.use((req, res, next) => {
-	req.requestTime = new Date().toISOString();
-	next();
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
 //Routes
@@ -30,6 +31,6 @@ app.use('/api/v1/users', userRouter);
 
 const port = 3000;
 app.listen(port, () => {
-	console.log(`App running on port ${port}....`);
+  console.log(`App running on port ${port}....`);
 });
 module.exports = app;
